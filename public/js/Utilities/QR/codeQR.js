09cc7d79b91c0fc02391fcda70fc6fc3b4ebcdd1
Ezequiel Blasi <profeblasi@gmail.com>
2020-11-25 00:04:07 -0300

    Falta token

--- public/js/Utilities/QR/codeQR.js
+++ public/js/Utilities/QR/codeQR.js
@@ -1,3 +1,52 @@
+/* Provisorio luego se va a añadir un token */
+let autorizado = false;
+
+let login = () =>
+{
+    swal({
+        text: 'Debe loguearse para trabajar en este sector".',
+        content: "input",
+        button: {
+          text: "Ingrese su contraseña",
+          /* closeModal: false, */
+        },
+      })
+      .then(name => {if (!name) throw null;
+        return fetch(`https://itunes.apple.com/search?term=${name}&entity=movie`);
+      })
+      .then(results => {
+        alert("Encontre tu pelicula");
+        autorizado = true;
+        return results.json();
+      })
+      .then(json => {
+        const movie = json.results[0];
+        if (!movie) {
+          swal("No movie was found!");
+          location.reload();
+        }
+       
+        const name = movie.trackName;
+        const imageURL = movie.artworkUrl100;
+       
+        swal({
+          title: "Top result:",
+          text: name,
+          icon: imageURL,
+        });
+      })
+      .catch(err => {
+        if (err) {
+          swal("Oh noes!", "The AJAX request failed!", "error");
+        } else {
+          swal.stopLoading();
+          swal.close();
+        }
+      });
+}
+
+login();
+
 $('#reader').html5_qrcode(function(data){ // función de lectura Ok
     alert(data);
     $('#reader').html5_qrcode_stop();},
@@ -15,9 +64,22 @@ btnBuscarCodigo.addEventListener('click', function(e){
      }).then((e) => {
          if(e)
          {
-             tracking(data);
+            if(autorizado==true)
+            {
+                tracking(data);
+            }
+            else
+            {
+                login();
+            }   
          }
      })
+});
+
+let btnRegistrarse = document.getElementById('registro');
+btnRegistrarse.addEventListener('click', function(e){
+    e.preventDefault();
+    login();
 });
 
 let tracking = (data) =>{
