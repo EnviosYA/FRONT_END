f416b9e666832f55516e918217ef41587541b61f
Daniel Villajuan <dvillajuan@andreani.com>
2020-11-30 20:56:02 -0300

    desarrollo perfil

--- public/js/Services/UsuarioService.js
+++ public/js/Services/UsuarioService.js
@@ -4,3 +4,7 @@ import {get, post} from "../Services/Requests.js";
 export const postUsuario = (usuario) => {
     return post(URLBASEUSUARIO, usuario);
 }
+
+export const getUsuario = (usuario) => {
+    return get(URLBASEUSUARIO +`?id=${usuario}`);
+}
