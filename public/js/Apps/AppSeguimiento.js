0cd8a3aec6274fca368b167160d4bcfac5be4e94
cirogargatagli <ciroshaila@gmail.com>
2020-12-08 16:41:54 -0300

    Pequeños cambios

--- public/js/Apps/AppSeguimiento.js
+++ public/js/Apps/AppSeguimiento.js
@@ -19,7 +19,7 @@ export const seguimiento = () => {
 
 const lectorQRSeguimiento = () =>{
     $('#reader').html5_qrcode(function (nroEnvio){      
-        getSucursalPorEnvio(nroEnvio);     
+        getSucursalPorEnvio(nroEnvio);
         $('#reader').html5_qrcode_stop();
         return;
     },
